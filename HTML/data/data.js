5163651eb5fdf28189c4b6d455c792b1c992e6dd
GiovannyBM <105441886+GiovannnyBM@users.noreply.github.com>
2022-09-12 16:15:11 -0500

    Add files via upload

---
+++ HTML/data/data.js
@@ -0,0 +1,28 @@
+export const data =
+[{
+    "id":1,
+    "name":"Manuela",
+    "image":"/imagenes/imagen1.png"
+},
+
+{
+    "id":2,
+    "name":"Vicepresidente Orville",
+    "image":"/imagenes/imagen2.jpg"
+},
+{
+    "id":3,
+    "name":"Anonymous",
+    "image":"/imagenes/imagen3.png"
+},
+{
+    "id":4,
+    "name":"Padre Laetonius",
+    "image":"/imagenes/imagen4.jpg"
+},
+{
+    "id":5,
+    "name":"Jairo",
+    "image":"/imagenes/imagen5.png"
+},
+]
